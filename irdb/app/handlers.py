605f36a4a25064ee3a7f8a779725093c2636c443
nickgeense <n.geense@solufo.com>
2021-04-14 14:53:00 +0200

    Added CRUD functions for main handler and redirect to swagger ui.
    Tests: Passing

--- irdb/app/handlers.py
+++ irdb/app/handlers.py
@@ -8,7 +8,38 @@ Module containing all the used Request Handlers for the Tornado Server.
 from tornado.web import RequestHandler
 
 
-class MainHandler(RequestHandler):
+class BaseHandler(RequestHandler):
 
-    def get(self):
-        pass
+    async def json_response(self, data, status_code=200):
+        self.set_status(status_code)
+        self.set_header("Content-Type", 'application/json')
+        self.write(data)
+
+    async def action_not_allowed(self):
+        await self.json_response({'message': 'request is not allowed'}, 403)
+
+    async def json_error(self):
+        await self.json_response({'message': 'body is empty'}, 404)
+
+
+class MainHandler(BaseHandler):
+    """Main Handler for service base url.
+
+    Handler will redirect requests to swagger ui to provide information about
+    the API.
+
+    Otherwise block other CRUD functions with not allowed status.
+    """
+
+    async def get(self):
+        self.redirect('/swagger/spec.html')
+        return
+
+    async def post(self, *args):
+        await self.action_not_allowed()
+
+    async def put(self, *args):
+        await self.action_not_allowed()
+
+    async def delete(self, *args):
+        await self.action_not_allowed()
--- tests/test_main_handler.py
+++ tests/test_main_handler.py
@@ -1,4 +1,6 @@
 import pytest
+import urllib
+import tornado
 
 from irdb.irdb import make_app
 
@@ -10,5 +12,39 @@ def app():
 
 @pytest.mark.gen_test
 def test_main_handler_get(http_client, base_url):
-    response = yield http_client.fetch(base_url)
-    assert response.code == 200
+    with pytest.raises(tornado.httpclient.HTTPClientError):
+        response = yield http_client.fetch(base_url, follow_redirects=False)
+
+        assert response.code == 302
+
+@pytest.mark.gen_test
+def test_main_handler_post(http_client, base_url):
+    test_data = {"Rock": "Stars"}
+
+    body = urllib.parse.urlencode(test_data)
+
+    with pytest.raises(tornado.httpclient.HTTPClientError):
+        response = yield http_client.fetch(base_url, method="POST", body=body)
+
+        assert response.code == 403
+
+@pytest.mark.gen_test
+def test_main_handler_put(http_client, base_url):
+    test_data = {"Rock": "Stars"}
+
+    body = urllib.parse.urlencode(test_data)
+
+    with pytest.raises(tornado.httpclient.HTTPClientError):
+        response = yield http_client.fetch(base_url, method="PUT", body=body)
+
+        assert response.code == 403
+
+@pytest.mark.gen_test
+def test_main_handler_delete(http_client, base_url):
+    # test_data = {"Rock": "Stars"}
+    #
+    # body = urllib.parse.urlencode(test_data)
+    with pytest.raises(tornado.httpclient.HTTPClientError):
+        response = yield http_client.fetch(base_url, method="DELETE")
+
+        assert response.code == 403
